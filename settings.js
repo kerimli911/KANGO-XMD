24ae454e9d8a23eb3d162d9df46e7116f90f1847
kerimli911 <96395537+kerimli911@users.noreply.github.com>
2025-05-17 17:35:21 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'KANGO~svlQUZ5J#Sk6gtClPDrOuEfcLtPzcYK0q-t1zxS_GFFBjp9Yq3Gk' 
 //Enter your KANGO-XMD session id here; must start with KANGO~
 
 //--------------------[ BOT NAME ]----------------------//
@@ -24,11 +24,11 @@ global.botname = process.env.BOT_NAME || 'KANGO-XMD'
 
 //-----------------[ OWNER NUMBER ]------------------//
 
-global.ownernumber = process.env.OWNER_NUMBER || '233509977126' 
+global.ownernumber = process.env.OWNER_NUMBER || '994508202214' 
 
 //--------------------[ SUDO ]--------------------------//
 
-global.sudo = process.env.SUDO ? process.env.SUDO.split(',') : ['233509977126', '233577860202'];
+global.sudo = process.env.SUDO ? process.env.SUDO.split(',') : ['994505545253', '994105215253'];
 // Type additional allowed users here
 //NB: They'll be able to use every functions of the bot without restrictions.
 
